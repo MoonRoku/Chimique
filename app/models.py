66e838bac94ffc462d7e753f0820abf5ca924f45
Lucas Fernando Silva Alves Novais <13722447607@MUZAMBINHO.EDU.BR>
2024-07-24 12:27:52 -0300

    Arrumando imagem

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
     origem = models.TextField()
-    imagem = models.ImageField(upload_to='app/static/imagens/', blank=True)
+    imagem = models.ImageField(upload_to='app/static/imagens', blank=True)
     compostos = models.TextField()
     categoria = models.CharField(max_length=100)
     class Meta:
--- config/settings.py
+++ config/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(STATIC_URL, "static")
+STATIC_URL = 'app/static/'
+STATIC_ROOT = os.path.join(STATIC_URL, "app/static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "app/static/"),
 ]
